9c69d456936d3f281d9dd9af440b0ffd98fd99e2
marxlow <marx_low94@hotmail.com>
2017-05-11 01:34:03 +0800

    Create new TTTState class to manage state of the game. Game is now playable in multiplayer mode.

--- src/TTTPlayer.java
+++ src/TTTPlayer.java
@@ -3,52 +3,109 @@ import java.util.Scanner;
 public class TTTPlayer {
     public static final String WELCOME_MESSAGE = "Bot: Hi, welcome to TicTacToe!";
     public static final String MODE_MESSAGE = "Bot: Choose your mode! Single player(S), Multi-player(M)\nS/M: ";
-    public static final String BEGIN_MESSAGE = "~~~~~~~BEGIN~~~~~~~";
-    public static final String ERROR_MESSAGE = "~~~~~~~ERROR~~~~~~~";
+    public static final String BEGIN_MESSAGE = "~~~~~~~~~~~~~~~~~~BEGIN~~~~~~~~~~~~~~~~~~";
+    public static final String ERROR_MESSAGE = "~~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~~";
+    public static final String GAMEOVER_MESSAGE = "~~~~~~~~~~~~~~~~GAME OVER~~~~~~~~~~~~~~~~";
     public static final String MULTI_PLAYER_MODE = "M";
     public static final String SINGLE_PLAYER_MODE = "S";
-    
-    String player_one_name;
-    String player_two_name;
-    String game_mode;
-    
+    public static final String REPLAY_YES = "Y";
+    public static final String REPLAY_NO = "N";
+
+    private String player_one;
+    private String player_two;
+    private String game_mode;
+
+    private int player_one_score = 0;
+    private int player_two_score = 0;
+
     public static void main(String[] args) {
-        TTTPlayer player= new TTTPlayer();
+        TTTPlayer player = new TTTPlayer();
         player.run();
     }
+
     // Runs the TTT game
     private void run() {
         System.out.println(WELCOME_MESSAGE);
-        // Check if single player or multiplayer mode.
         configureGameMode();
+        startGame();
     }
-    
+
+    private void startGame() {
+        // Generate randomly who goes first.
+        int turn = 0;
+        TTTState state = new TTTState(game_mode, player_one, player_two);
+        while (!state.hasLost()) {
+            // Continue playing
+            state.makeMove(turn);
+            turn++;
+            turn = turn % 2;
+
+        }
+        System.out.println(GAMEOVER_MESSAGE);
+        System.out.println("Winner: " + state.getWinner());
+        printScore(state.getWinner());
+
+        // Check if the user wants to replay.
+        if (replay()) {
+            startGame();
+        }
+    }
+
     private void configureGameMode() {
         Scanner sc = new Scanner(System.in);
         String mode;
         System.out.print(MODE_MESSAGE);
         mode = sc.nextLine().trim().toUpperCase();
-        
+
         if (mode.equals(MULTI_PLAYER_MODE)) {
             game_mode = MULTI_PLAYER_MODE;
             System.out.println("Bot: You have selected Multi-player mode");
             System.out.print("Bot: Enter Player One name: ");
-            player_one_name = sc.nextLine();
+            player_one = sc.nextLine();
             System.out.print("Bot: Enter Player Two name: ");
-            player_two_name = sc.nextLine();
-            System.out.print("Bot: " + player_one_name + " VERSUS " + player_two_name);
-            
+            player_two = sc.nextLine();
+
         } else if (mode.equals(SINGLE_PLAYER_MODE)) {
             game_mode = SINGLE_PLAYER_MODE;
             System.out.println("Bot: You have selected Single player mode");
             System.out.print("Bot: Enter Player name: ");
-            player_one_name = sc.nextLine();
+            player_one = sc.nextLine();
+            player_two = "Bot";
+
         } else {
             System.out.println(ERROR_MESSAGE);
             System.out.print("Invalid game mode, please try 'S' or 'M'\n");
             System.out.println(ERROR_MESSAGE + "\n");
             configureGameMode();
         }
+
+        // System.out.println(player_one + " VERSUS " + player_two);
         System.out.println(BEGIN_MESSAGE);
     }
+
+    // Should only be called after a game has ended.
+    // Prints the total score for each player.
+    private void printScore(String winner) {
+        if (winner.equals(player_one)) {
+            player_one_score++;
+        } else if (winner.equals(player_two)) {
+            player_two_score++;
+        }
+        System.out.println(player_one + " score: " + player_one_score);
+        System.out.println(player_two + " score: " + player_two_score);
+    }
+
+    // Returns true if player wants to replay again.
+    private boolean replay() {
+        String answer;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Bot: Type 'y' or 'Y' to play again: ");
+        answer = sc.nextLine().trim().toUpperCase();
+        if (answer.equals(REPLAY_YES)) {
+            return true;
+        }
+        return false;
+    }
+
 }
---
+++ src/TTTState.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+
+public class TTTState {
+    
+    public static final int EMPTY = 2;
+    public static final int PLAYER_ONE_MOVE = 0;
+    public static final int PLAYER_TWO_MOVE = 1;
+    public static final String SEPARATOR = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    
+    private String game_mode;
+    private String player_one;
+    private String player_two;
+    private String current_player;
+    private String game_winner;
+    private int[][] game_state = new int[3][3];
+    private int move;
+    private int move_counts = 0;
+    private boolean hasLost = false;
+    private Scanner sc = new Scanner(System.in);
+    
+    public TTTState(String game_mode, String player_one, String player_two) {
+        this.game_mode = game_mode;
+        this.player_one = player_one;
+        this.player_two = player_two;
+        initGameState();
+    }
+    
+    public void makeMove(int turn) {
+        initPlayer(turn); // get the current player
+        printState();
+        getMove();
+        recordMove(turn);
+        updateState();
+    }
+    
+    public boolean hasLost() {
+        return hasLost;
+    }
+    
+    public String getWinner() {
+        return game_winner;
+    }
+    
+    // prompts the player to make a move based on the current game state
+    private void getMove() {
+        System.out.print("Bot: " + current_player + "'s turn, please make a move between [0-8]: ");
+        try {
+            move = sc.nextInt();
+            checkMove();
+        } catch (Exception e) {
+            // ILLEGAL VALUES given by player, repeat prompt
+            System.out.println(TTTPlayer.ERROR_MESSAGE);
+            System.out.print("Invalid move, please only try numbers in current state\n");        
+            System.out.println(TTTPlayer.ERROR_MESSAGE + "\n");
+            getMove();
+        }
+        
+    }
+    
+    // record down the move of a player in the game state
+    private void recordMove(int turn) {
+        int row = move / 3;
+        int col = move % 3;
+        if (turn == PLAYER_ONE_MOVE) {
+            game_state[row][col] = PLAYER_ONE_MOVE;
+        } else {
+            game_state[row][col] = PLAYER_TWO_MOVE;
+        }
+        return;
+    }
+    
+   // Checks for end game
+    private void updateState() {
+        move_counts ++;
+        if (move_counts == 9) {
+            hasLost= true;
+            game_winner = "DRAW";
+        }
+        if (checkWinner() == EMPTY) {
+            hasLost = false;
+        } else if (checkWinner() == PLAYER_ONE_MOVE) {
+            game_winner = player_one;
+            hasLost = true;
+        } else if (checkWinner() == PLAYER_TWO_MOVE) {
+            game_winner = player_two;
+            hasLost = true;
+        }
+    }
+    
+    // Returns the index of the winner(if any)
+    // Returns EMPTY(2) if there are still no winners
+    private int checkWinner() {
+       // Check for horizontal wins.
+        for (int row = 0; row < 3; row ++) {
+            if (game_state[row][0] == game_state[row][1] && game_state[row][0] == game_state[row][2]) {
+                return game_state[row][0];
+            }
+        }
+        // Check for vertical wins.
+        for (int col = 0; col < 3; col++) {
+            if (game_state[0][col] == game_state[1][col] && game_state[0][col] == game_state[2][col]) {
+                return game_state[0][col];
+            }
+        }
+        // Check for "\" wins.
+        if (game_state[0][0] == game_state[1][1] && game_state[0][0] == game_state[2][2]) {
+            return game_state[0][0];
+        }
+        // Check for "/" wins.
+        if (game_state[0][2] == game_state[1][1] && game_state[0][2] == game_state[2][0]) {
+            return game_state[0][2];
+        }
+        return EMPTY;
+    }
+    
+    // Returns true if move is valid
+    private void checkMove() throws Exception {
+        int row = move / 3;
+        int col = move % 3;
+        
+        if (game_state[row][col] != EMPTY) {
+            throw new Exception();
+        }
+    }
+    
+    // Prints the current image of the TTT state
+    private void printState() {
+        StringBuilder sb = new StringBuilder();
+        int position;
+        
+        sb.append("\n" + SEPARATOR + "\n\n");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (game_state[row][col] == EMPTY) {
+                    position = (row * 3) + col;
+                    sb.append(" " + String.valueOf(position) + " ");
+                } else if (game_state[row][col] == PLAYER_ONE_MOVE) {
+                        sb.append(" O ");
+                } else if (game_state[row][col] == PLAYER_TWO_MOVE) {
+                        sb.append(" X ");
+                }
+                sb.append("|");
+            }
+            sb.append("\n------------\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
+    
+    // initialize gameState to all be empty cells
+    private void initGameState() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                game_state[row][col] = EMPTY;
+            }
+        }
+    }
+    // initialize who is the current player making a move
+    private void initPlayer(int turn) {
+        if (turn == PLAYER_ONE_MOVE) {
+            this.current_player = player_one;
+        } else {
+            this.current_player = player_two;
+        }
+    }
+}
